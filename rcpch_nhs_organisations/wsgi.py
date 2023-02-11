b11fa304c64ba8719f3dc3d433d9009bc2fc6f26
Simon Chapman <eatyourpeasapps@gmail.com>
2023-02-11 15:46:31 +0000

    initial commit

---
+++ rcpch_nhs_organisations/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rcpch_nhs_organisations project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rcpch_nhs_organisations.settings')
+
+application = get_wsgi_application()
