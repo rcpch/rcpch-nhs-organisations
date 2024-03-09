2b09e4e6a51dea625e53a84a98dd42f1509cc078
eatyourpeas <simon@eatyourpeas.co.uk>
2024-03-09 10:36:30 +0000

    remove celery and dependencies

--- rcpch_nhs_organisations/hospitals/tasks.py
+++ rcpch_nhs_organisations/hospitals/tasks.py
@@ -1,9 +1,7 @@
 # Third party imports
-from celery import shared_task
 from .general_functions import update_organisation_model_with_ORD_changes
 
 
-@shared_task
 def poll_ORD_spineservices_update_organisations_and_trusts():
     """
     polls ORD spineservices API and updates Organisation and Trust objects
