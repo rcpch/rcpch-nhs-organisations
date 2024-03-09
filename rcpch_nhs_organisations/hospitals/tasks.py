041b56aa16eb36545e87f815e8ce416c99ddcec2
eatyourpeas <simon@eatyourpeas.co.uk>
2024-03-09 20:26:03 +0000

    move tasks to management function

--- rcpch_nhs_organisations/hospitals/tasks.py
+++
@@ -1,9 +0,0 @@
-# Third party imports
-from .general_functions import update_organisation_model_with_ORD_changes
-
-
-def poll_ORD_spineservices_update_organisations_and_trusts():
-    """
-    polls ORD spineservices API and updates Organisation and Trust objects
-    """
-    update_organisation_model_with_ORD_changes()
