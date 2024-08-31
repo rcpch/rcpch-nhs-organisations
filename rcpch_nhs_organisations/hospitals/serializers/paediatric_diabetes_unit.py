3c5cdcd56c73dd8b65f5309f70eba192d70c3eaa
eatyourpeas <simon@eatyourpeas.co.uk>
2024-08-31 19:46:35 +0100

    update paediatric diabetes unit / extended endpoint to return nested networks

--- rcpch_nhs_organisations/hospitals/serializers/paediatric_diabetes_unit.py
+++ rcpch_nhs_organisations/hospitals/serializers/paediatric_diabetes_unit.py
@@ -4,6 +4,8 @@ from drf_spectacular.utils import extend_schema_serializer, OpenApiExample
 
 from ..models import PaediatricDiabetesUnit
 
+from .paediatric_diabetes_network import PaediatricDiabetesNetworkSerializer
+
 
 @extend_schema_serializer(
     examples=[
@@ -11,16 +13,25 @@ from ..models import PaediatricDiabetesUnit
             "/paediatric_diabetes_unit/1/",
             value={
                 "pz_code": "",
+                "paediatric_diabetes_network": {"pn_code": "", "name": ""},
             },
             response_only=True,
         )
     ]
 )
 class PaediatricDiabetesUnitSerializer(serializers.ModelSerializer):
+    paediatric_diabetes_network = serializers.SerializerMethodField()
 
     class Meta:
         model = PaediatricDiabetesUnit
         # depth = 1
         fields = [
             "pz_code",
+            "paediatric_diabetes_network",
         ]
+
+    def get_paediatric_diabetes_network(self, obj):
+        network = obj.paediatric_diabetes_network
+        if network:
+            return PaediatricDiabetesNetworkSerializer(network).data
+        return None
