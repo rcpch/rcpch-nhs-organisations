2256ec8f586024d015434523e5813e41052c7a3d
eatyourpeas <simon@eatyourpeas.co.uk>
2024-10-19 14:08:58 +0100

    remove abstractuser as too hard. add admin interface

--- rcpch_nhs_organisations/hospitals/admin.py
+++ rcpch_nhs_organisations/hospitals/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import (
+from .models_folder import (
     LocalHealthBoard,
     Organisation,
     PaediatricDiabetesNetwork,
