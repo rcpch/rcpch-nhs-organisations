72ccd3a707430d3a18bdfc8235ae01138147e409
eatyourpeas <simon@eatyourpeas.co.uk>
2024-10-19 14:27:14 +0100

    oops models_folder refactor fail

--- rcpch_nhs_organisations/hospitals/admin.py
+++ rcpch_nhs_organisations/hospitals/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models_folder import (
+from .models import (
     LocalHealthBoard,
     Organisation,
     PaediatricDiabetesNetwork,
